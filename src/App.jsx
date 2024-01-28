810d7ba651c0788ed952289ab7fec5c652ef72e0
Jawad Awan <101397741+jawad989@users.noreply.github.com>
2024-01-28 13:47:08 +0500

    UI fixes

--- src/App.jsx
+++ src/App.jsx
@@ -180,81 +180,67 @@ function App() {
       </section>
 
       {/* Footer */}
-      <footer class='bg-veryDarkBlue'>
+      <footer className='bg-veryDarkBlue'>
         {/* Flex Container */}
-        <div class='container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0'>
+        <div className='container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0'>
           {/* Logo and social links container */}
-          <div class='flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start'>
-            <div class='mx-auto my-6 text-center text-white md:hidden'>
+          <div className='flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start'>
+            <div className='mx-auto my-6 text-center text-white md:hidden'>
               Copyright &copy; 2022, All Rights Reserved
             </div>
             {/* Logo */}
             <div>
-              <img src='img/logo-white.svg' class='h-8' alt='' />
+              <img src='img/logo-white.svg' className='h-8' alt='' />
             </div>
             {/* Social Links Container */}
-            <div class='flex justify-center space-x-4'>
+            <div className='flex justify-center space-x-4'>
               <a href='#'>
-                <img src='img/icon-facebook.svg' alt='' class='h-8' />
+                <SocialLink profileAvatar='img/icon-facebook.svg' />
               </a>
               <a href='#'>
-                <img src='img/icon-youtube.svg' alt='' class='h-8' />
+                <SocialLink profileAvatar='img/icon-youtube.svg' />
               </a>
               <a href='#'>
-                <img src='img/icon-twitter.svg' alt='' class='h-8' />
+                <SocialLink profileAvatar='img/icon-twitter.svg' />
               </a>
               <a href='#'>
-                <img src='img/icon-pinterest.svg' alt='' class='h-8' />
+                <SocialLink profileAvatar='img/icon-pinterest.svg' />
               </a>
               <a href='#'>
-                <img src='img/icon-instagram.svg' alt='' class='h-8' />
+                <SocialLink profileAvatar='img/icon-instagram.svg' />
               </a>
             </div>
           </div>
           {/* List Container */}
-          <div class='flex justify-around space-x-32'>
-            <div class='flex flex-col space-y-3 text-white'>
-              <a href='#' class='hover:text-brightRed'>
-                Home
-              </a>
-              <a href='#' class='hover:text-brightRed'>
-                Pricing
-              </a>
-              <a href='#' class='hover:text-brightRed'>
-                Products
-              </a>
-              <a href='#' class='hover:text-brightRed'>
-                About
-              </a>
+          <div className='flex justify-around space-x-32'>
+            <div className='flex flex-col space-y-3 text-white'>
+              <FooterLink linkLabel='Home' />
+              <FooterLink linkLabel='Pricing' />
+              <FooterLink linkLabel='Products' />
+              <FooterLink linkLabel='About' />
             </div>
-            <div class='flex flex-col space-y-3 text-white'>
-              <a href='#' class='hover:text-brightRed'>
-                Careers
-              </a>
-              <a href='#' class='hover:text-brightRed'>
-                Community
-              </a>
-              <a href='#' class='hover:text-brightRed'>
-                Privacy Policy
-              </a>
+            <div className='flex flex-col space-y-3 text-white'>
+              <FooterLink linkLabel='Careers' />
+              <FooterLink linkLabel='Community' />
+              <FooterLink linkLabel='Privacy Policy' />
             </div>
           </div>
 
           {/* Input Container */}
-          <div class='flex flex-col justify-between'>
+          <div className='flex flex-col justify-between'>
             <form>
-              <div class='flex space-x-3'>
+              <div className='flex space-x-3'>
                 <input
                   type='text'
-                  class='flex-1 px-4 rounded-full focus:outline-none'
+                  className='flex-1 px-4 rounded-full focus:outline-none'
                   placeholder='Updated in your inbox'
                 />
-                <button class='px-6 py-2 text-white rounded-full bg-brightRed hover:bg-brightRedLight focus:outline-none'>
+                <button className='px-6 py-2 text-white rounded-full bg-brightRed hover:bg-brightRedLight focus:outline-none'>
                   Go
                 </button>
               </div>
             </form>
-            <div class='hidden text-white md:block'>
+            <div className='hidden text-white md:block'>
               Copyright &copy; 2022, All Rights Reserved
             </div>
           </div>
